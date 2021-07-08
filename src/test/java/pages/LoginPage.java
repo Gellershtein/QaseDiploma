912d9fec6d8f64099811a5f5e9423259849f3864
Gellershtein <63881048+Gellershtein@users.noreply.github.com>
2021-07-08 19:32:06 +0300

    Change log: - added Data provider for Invalid data - added negative tests for LoginPage

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,13 +4,20 @@ import com.codeborne.selenide.Selenide;
 import elements.Button;
 import elements.Input;
 import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 import pages.base.BasePage;
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static org.testng.Assert.assertEquals;
 
 public class LoginPage extends BasePage {
 
+    public static final By ERROR_MESSAGE_LOCATOR = By.className("form-control-feedback");
+    public static final String INVALID_CREDENTIALS_ERROR_MESSAGE = "These credentials do not match our records.";
+
     public LoginPage fillLoginForm(String email, String password) {
+
         new Input("Email").write(email);
         new Input("Password").write(password);
         new Button("Login").click();
@@ -21,6 +28,12 @@ public class LoginPage extends BasePage {
     public ProjectsListPage login(String email, String password) {
         fillLoginForm(email, password);
         return new ProjectsListPage();
+    }
+
+    @Step("Filling login form")
+    public LoginPage invalidLogin(String email, String password) {
+        fillLoginForm(email, password);
+        return this;
     }
 
     @Override
@@ -35,4 +48,16 @@ public class LoginPage extends BasePage {
         Selenide.open("login");
         return this;
     }
+
+
+    public LoginPage isErrorAppeared() {
+        $(ERROR_MESSAGE_LOCATOR).shouldBe(visible);
+        return this;
+    }
+
+    public LoginPage validateErrorMessage(String errorMessage) {
+        assertEquals(getTextOfElement(ERROR_MESSAGE_LOCATOR), errorMessage, "Error does not match as expected");
+        return this;
+    }
+
 }
--- src/test/java/pages/base/AbstractPage.java
+++ src/test/java/pages/base/AbstractPage.java
@@ -1,6 +1,20 @@
 package pages.base;
 
+import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+@Log4j2
 public abstract class AbstractPage {
 
     public abstract AbstractPage isOpened();
+
+    public String getTextOfElement(By locator) {
+        log.debug(String.format("Getting text for element with locator '%s'", locator));
+        String text = $(locator).shouldBe(visible).getText();
+        log.debug(String.format("Text of element %s", text));
+        return text;
+    }
 }
