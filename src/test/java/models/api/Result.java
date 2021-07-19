189873ed482f5cd87c5eebf4e8eae9d7b82be772
Gellershtein <63881048+Gellershtein@users.noreply.github.com>
2021-07-19 16:17:00 +0300

    Change log: - added SuiteAdapter and other related classes - added API methods to SuiteSteps - added APISuiteTest

--- src/test/java/models/api/Result.java
+++ src/test/java/models/api/Result.java
@@ -7,6 +7,7 @@ import lombok.Data;
 @Builder
 @Data
 public class Result<T> {
+    @Expose
     boolean status;
     @Expose
     T result;
---
+++ src/test/java/tests/api/APISuiteTest.java
@@ -0,0 +1,55 @@
+package tests.api;
+
+import factories.ProjectFactory;
+import factories.SuiteFactory;
+import lombok.extern.log4j.Log4j2;
+import models.Project;
+import models.Suite;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import steps.ProjectsSteps;
+import steps.SuiteSteps;
+
+@Log4j2
+public class APISuiteTest {
+    ProjectsSteps projectsSteps = new ProjectsSteps();
+    SuiteSteps suiteSteps = new SuiteSteps();
+    Project newProject;
+    Suite newSuite, updateSuite;
+
+    @BeforeMethod(alwaysRun = true, description = "Create project before test")
+    public void createNewProject() {
+        ProjectFactory projectFactory = new ProjectFactory();
+        newProject = projectFactory.getProject();
+        SuiteFactory suiteFactory = new SuiteFactory();
+        newSuite = suiteFactory.getSuite();
+        updateSuite = suiteFactory.getSuite();
+
+        projectsSteps
+                .createNewProjectViaApi(newProject);
+    }
+
+    @Test(description = "Get suite from project: {newProject.title} by ID and validate it")
+    public void getSuiteByIdViaApi() {
+        suiteSteps
+                .createNewSuiteViaApi(newProject, newSuite)
+//                .getSuiteWithIdAndValidateItViaApi(newProject, newSuite, 1)
+                .updateSuiteByIdViaApi(newProject, updateSuite, 1)
+                .getAllSuitesUsingAPI(newProject)
+                .deleteCaseByIdViaApi(newProject, 1);
+    }
+
+    @AfterMethod(description = "Delete project after test")
+    public void deleteProject(ITestResult result) {
+        projectsSteps
+                .deleteProjectViaApi(newProject);
+//        if (result.getStatus() == ITestResult.SUCCESS) {
+//            projectsSteps
+//                    .deleteProjectViaApi(newProject);
+//        } else {
+//            log.debug("Test was FAILED, project {} still alive for debugging", newProject.getTitle());
+//        }
+    }
+}
